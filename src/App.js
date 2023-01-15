9606969ab95a08d0c9834ed3494594541363d7fb
Chen Levin <henc1995@gmail.com>
2023-01-15 21:25:42 +0200

    test1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello
+        Hello world
       </header>
     </div>
   );
